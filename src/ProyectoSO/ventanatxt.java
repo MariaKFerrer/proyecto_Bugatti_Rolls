76dce033194956708f9de79dd7b4ccb59284ed34
marselo95 <97412737+marselo95@users.noreply.github.com>
2023-06-01 21:11:20 -0400

    update

--- src/ProyectoSO/ventanatxt.java
+++ src/ProyectoSO/ventanatxt.java
@@ -65,6 +65,11 @@ public class ventanatxt extends javax.swing.JPanel {
 
         jButton1.setForeground(new java.awt.Color(0, 102, 153));
         jButton1.setText("Modificar");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
         add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(180, 420, -1, -1));
 
         jLabel3.setForeground(new java.awt.Color(255, 255, 255));
@@ -74,6 +79,10 @@ public class ventanatxt extends javax.swing.JPanel {
         jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagen/Sirve2.png"))); // NOI18N
         add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, -1, 488));
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButton1ActionPerformed
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
